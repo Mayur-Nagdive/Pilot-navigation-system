8b04e55514375986b08a28d5a45df5034e8b1ec5
Mayur Nagdive <81313227+Mayur-Nagdive@users.noreply.github.com>
2021-03-25 17:29:37 +0530

    Add files via upload
    This is the pilot navigation assignment given on day 3 in let's upgrade 'python essential' day 3 program. 
    -Mayur Nagdive

---
+++ pilot_navigation_system.py
@@ -0,0 +1,35 @@
+#Pilot navigation system
+#you are the pilot and your plane is at the altitude of 3000ft which is safe for landing
+#if the plane is above 3000ft and 6000ft atleast allow the pilot to try for landing
+#if your plane is above 6000ft din't allow the  pilot to land the plane and ask to go around
+#help him land the plane
+#user will enter the altitude of the plane
+#---------------------------------------------------------------------------------------------------
+#Author: MAyur Nagdive
+#Date:24 March 2021
+
+#Pilot Navigation system
+a=int(input('Enter the altitude of the plane in fts \n'))
+
+if a<=3000:
+    
+    print('your are same to land')
+
+    s=float(input('enter the speed of the plane in m/s \n'))
+    if s<=800:
+      print('Maintain your speed at 800 m/s while landing')
+    else:
+      print('Please lower your speed to 800 m/s')
+
+elif 3000<=a<=6000:
+    print('Be cautious but you can land at will')
+
+    s=float(input('enter the speed of the plane in m/s \n'))
+    if s<=800:
+      print('Maintain your speed while landing')
+    else:
+      print('Please lower your speed to 800 m/s')
+
+else:
+    print('please take a roundabout and lower your altitude')
+
